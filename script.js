ac953d9d7056cadb90e15bb2fc563cceaac53957
Farhan-Khan7 <fk5518744@gmail.com>
2026-06-17 03:37:39 +0530

    I have done 20%  UI

--- script.js
+++ script.js
@@ -1,14 +1,26 @@
-const today = new Date()
+const changetext = [
+    "dynamic teams",
+    "increased productivity",
+    "improved collaboration",
+    "smart workflows",
+    "better organization",
+    "You"
+];
 
-const format = {
-    weekday : "long",
-    month : "long",
-    day : "numeric"
-}
+const text = document.querySelector("#changing-text");
 
-const formatDate = today.toLocaleDateString("en-US" , format)
+let index = 0;
 
+setInterval(() => {
+    index++;
 
-let currentDate = document.querySelector("#current-date");
+    if(index >= changetext.length){
+        index = 0;
+    }
 
-currentDate.textContent = formatDate.toUpperCase()
+    text.style.animation = "none"
+    text.offsetHeight;
+    text.textContent = changetext[index];
+    text.style.animation = "dropIn 0.6s ease"
+    
+}, 2000)
